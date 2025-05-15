ce91d916c3d0f133237c96f93277df9c7861a8ab
shawndhillon <gdhill11@ucsc.edu>
2025-05-14 19:12:56 -0700

    more shit

--- demos/js/lab.js
+++ demos/js/lab.js
@@ -1,18 +1,52 @@
 function openPopup() {
-  document.getElementById("popup1").style.display = "block";
+  document.getElementById("popup").style.display = "block";
 }
 
 function openPopup2() {
-  document.getElementById("popup1").style.display = "none";
-  document.getElementById("popup2").style.display = "block";
+  document.getElementById("popup").style.display = "none";
+
+  const popup = document.getElementById("popup");
+  const popup2 = document.getElementById("popup2");
+
+  popup2.style.top = popup.style.top;
+  popup2.style.left = popup.style.left;
+
+  popup2.style.display = "block";
 }
 
 function closeAll() {
-  document.getElementById("popup1").style.display = "none";
-  document.getElementById("popup2").style.display = "none";
+  const popup = document.getElementById("popup");
+  if (popup) {
+    popup.style.display = "none";
+  }
+
+  const popup2 = document.getElementById("popup2");
+  if (popup2) {
+    popup2.style.display = "none";
+  }
+
+  const overlay = document.getElementById("overlay");
+  if (overlay) {
+    overlay.style.display = "none";
+  }
 }
 
+function openRandomPopup() {
+  const overlay = document.getElementById("overlay");
+  const popup = document.getElementById("popup");
 
+  const maxX = window.innerWidth - 400;
+  const maxY = window.innerHeight - 400;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  popup.style.left = `${randomX}px`;
+  popup.style.top = `${randomY}px`;
+
+  overlay.style.display = "block";
+  popup.style.display = "block";
+}
 
 
 // click listener for button
